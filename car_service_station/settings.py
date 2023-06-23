a38dae271161e71822220f3655007e429276cdf8
Dmytro Shvets <dirolius@gmail.com>
2023-06-23 18:57:46 +0300

    install django debug toolbar

--- car_service_station/settings.py
+++ car_service_station/settings.py
@@ -44,10 +44,12 @@ INSTALLED_APPS = [
     "crispy_forms",
     "crispy_bootstrap5",
     "service",
+    "debug_toolbar",
 ]
 
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
+    "debug_toolbar.middleware.DebugToolbarMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
     "django.middleware.csrf.CsrfViewMiddleware",
