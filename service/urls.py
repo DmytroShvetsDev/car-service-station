a562e5755da32bf36410f5435fe01ada26bf1e3d
Dmytro Shvets <dirolius@gmail.com>
2023-06-23 12:12:13 +0300

    Add CRUD urls and viewv for Profession model

--- service/urls.py
+++ service/urls.py
@@ -1,9 +1,30 @@
 from django.urls import path
 
-from service.views import index
+from service.views import index, ProfessionsListView, ProfessionsCreateView, ProfessionUpdateView, ProfessionDeleteView
 
 urlpatterns = [
     path("", index, name="index"),
+    path(
+        "professions/",
+        ProfessionsListView.as_view(),
+        name="professions-list",
+    ),
+    path(
+        "professions/create/",
+        ProfessionsCreateView.as_view(),
+        name="profession-create",
+    ),
+    path(
+        "professions/<int:pk>/update/",
+        ProfessionUpdateView.as_view(),
+        name="profession-update",
+    ),
+    path(
+        "professions/<int:pk>/delete/",
+        ProfessionDeleteView.as_view(),
+        name="profession-delete",
+    ),
+
 ]
 
 
--- service/views.py
+++ service/views.py
@@ -1,5 +1,8 @@
 from django.contrib.auth.decorators import login_required
+from django.contrib.auth.mixins import LoginRequiredMixin
 from django.shortcuts import render
+from django.urls import reverse_lazy
+from django.views import generic
 
 from service.models import Vehicle, Worker, Task, TaskType, Profession
 
@@ -24,3 +27,27 @@ def index(request):
     }
 
     return render(request, "service/index.html", context=context)
+
+
+class ProfessionsListView(LoginRequiredMixin, generic.ListView):
+    model = Profession
+    context_object_name = "professions_list"
+    template_name = "service/professions_list.html"
+    queryset = Profession.objects.all()
+
+
+class ProfessionsCreateView(LoginRequiredMixin, generic.CreateView):
+    model = Profession
+    fields = "__all__"
+    success_url = reverse_lazy("service:professions-list")
+
+
+class ProfessionUpdateView(LoginRequiredMixin, generic.UpdateView):
+    model = Profession
+    fields = "__all__"
+    success_url = reverse_lazy("service:professions-list")
+
+
+class ProfessionDeleteView(LoginRequiredMixin, generic.DeleteView):
+    model = Profession
+    success_url = reverse_lazy("service:professions-list")
