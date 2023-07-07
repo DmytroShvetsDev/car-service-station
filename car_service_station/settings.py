b7baa6135a053c7c54c30890bf121ccf48dcb88a
Dmytro Shvets <dirolius@gmail.com>
2023-07-07 18:03:06 +0300

    removed insecure Django secret key from settings.py

--- car_service_station/settings.py
+++ car_service_station/settings.py
@@ -13,6 +13,10 @@ import os
 from pathlib import Path
 
 import dj_database_url
+from dotenv import load_dotenv
+
+load_dotenv()
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -22,7 +26,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get("DJANGO_SECRET_KEY", "django-insecure-auvd$y!g91q3tq_&@vbabrp0olv867&r)@9d^2z+l5#@ar$c=l")
+SECRET_KEY = os.environ["DJANGO_SECRET_KEY"]
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get("DJANGO_DEBUG", "") != "False"
